5b4623619bd5414ba2677c7bf7c7a19124df3771
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-04-26 13:45:29 +1000

    set up ticket end point

---
+++ src/endpoints/ticket.js
@@ -0,0 +1,28 @@
+const errorHandler = require('../helpers/errorHandler.js')
+const responseHandler = require('../helpers/responseHandler.js')
+
+const Tickets = (api = null) => {
+  if (api === null) throw new Error('Request instance must be provided on constructor.')
+
+  return {
+    mapProperties(properties) {
+      return Object.keys(properties).map(property => ({
+        name: property,
+        value: properties[property]
+      }))
+    },
+
+    getAll(params) {
+      const defaultParams = {
+        offset: null,
+        ...params
+      }
+
+      return api.get('crm-objects/v1/objects/tickets/paged', defaultParams)
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    }
+  }
+}
+
+module.exports = Tickets
