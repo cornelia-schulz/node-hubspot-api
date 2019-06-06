078ee9d9e1ff88d8fda37d54d7625dff420c923a
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-06-06 11:23:00 +1000

    add files api

---
+++ dist/endpoints/files.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];for (var key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                target[key] = source[key];
+            }
+        }
+    }return target;
+};
+
+var errorHandler = require('../helpers/errorHandler.js');
+var responseHandler = require('../helpers/responseHandler.js');
+
+/* files = {'files': open('/path/on/your/local-computer/report.xls', 'rb')}
+r = requests.post(
+    'http://api.hubapi.com/filemanager/api/v2/files?hapikey=demo', 
+    data={
+        "folder_paths": "folder-in-the-cos-file-manager/subfolder"
+         },
+    files=files)
+    */
+
+var Files = function Files() {
+    var api = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+
+    if (api === null) throw new Error('Request instance must be provided on constructor.');
+
+    return {
+
+        mapProperties: function mapProperties(properties) {
+            return Object.keys(properties).map(function (property) {
+                return {
+                    name: property,
+                    value: properties[property]
+                };
+            });
+        },
+        uploadFile: function uploadFile(properties) {
+            // var mappedProperties = this.mapProperties(properties)
+            return api.post2('filemanager/api/v2/files', data = prooerties.data).then(function (response) {
+                console.log(response);
+            }).catch(function (error) {
+                console.log('error: ', error);
+                errorHandler(error);
+            });
+        }
+    };
+};
+
+module.exports = Files;
